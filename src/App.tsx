ac0936773a9283c635792bad55f912d33b725f14
kast96 <kast96@yandex.ru>
2022-08-26 09:48:41 +0300

    fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react';
 import './assets/scss/app.scss'
 import { Categories } from './components/Categories';
 import { Header } from './components/Header';
 import { PizzaBlock } from './components/PizzaBlock';
 import { Sort } from './components/Sort';
-import pizzas from './assets/pizzas.json';
+
+type ItemsType = {
+  id: number
+  imageUrl: string
+  title: string
+  types: Array<number>
+  sizes: Array<number>
+  price: number
+  category: number
+  rating: number
+}
 
 const App = () => {
+  const [items, setItems] = useState<Array<ItemsType>>([])
+
+  useEffect(() => {
+    fetch('https://63085e6b722029d9ddcd2b4a.mockapi.io/items').then(response => response.json()).then(json => setItems(json))
+  }, [])
+
   return (
     <div className="wrapper">
       <Header />
@@ -17,7 +34,7 @@ const App = () => {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map(pizza => 
+            {items.map(pizza => 
               <PizzaBlock key={pizza.id} title={pizza.title} price={pizza.price} imageUrl={pizza.imageUrl} sizes={pizza.sizes} types={pizza.types} />
             )}
           </div>
